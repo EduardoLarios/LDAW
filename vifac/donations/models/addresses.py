a2cdd11424248910e1a0b727837d2ddc1a2311fb
EduardoLarios <eduardo.larios.fdz@outlook.com>
2017-02-25 12:52:26 -0600

    New development branch

--- vifac/donations/models/addresses.py
+++ vifac/donations/models/addresses.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-class Address(models.Model): pass
+class Address(models.Model):
+	street = models.CharField(
+		max_length = 256,
+		null = False,
+		blank = False,
+		default = ''
+	)
