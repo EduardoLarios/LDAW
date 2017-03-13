b20999b49baf1114cbc0c599e85d6f9e3ee87210
Esteban <a01184855@itesm.mx>
2017-03-12 22:35:59 -0600

    Category and Donations form and view

--- VIFAC/donors/views.py
+++ VIFAC/donors/views.py
@@ -1,9 +1,12 @@
 from .Forms.donors import DonorForm
+from .Forms.donations import DonationForm
 from django.shortcuts import render
 from django.template import loader
 import datetime
 from .models.donors import Donor
-
+from .models.donations import Donation
+from .Forms.categories import CategoriesForm
+from .models.categories import Category
 
 def index(request):
     context = {}
@@ -66,3 +69,46 @@ def new_donor(request):
         new_donor_form = DonorForm()
     
     return render(request, 'donor_created.html', context)
+
+def new_donation(request):
+    if request.method == "POST":
+        #Form through POST
+        new_donation_form = DonationForm(request.POST)
+        
+        if new_donation_form.is_valid():
+        
+            #Variables
+            description = new_donation_form.cleaned_data['description']
+            category =  new_donation_form.cleaned_data['category']
+            donor = new_donation_form.cleaned_data['donor']
+            
+            donation = Donation.create(
+                description = description,
+                category = category,
+                donor = donor
+            )
+            
+            donation.save()
+            
+    else:
+        new_donation_form = DonationForm()
+        
+def new_category(request):
+    if request.method == "POST":
+        #Form through POST
+        new_category_form = CategoriesForm(request.POST)
+        
+        if new_category_form.is_valid():
+            
+            name =  new_category_form.cleaned_data['name']
+            description = new_category_form.cleaned_data['description']
+            
+            category = Category.create(
+                name = name,
+                description = description
+            )
+            
+            category.save()
+            
+    else:
+        new_category_form = CategoriesForm()
