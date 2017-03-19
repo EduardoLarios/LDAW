0a8c301089e78634a630b3a381d29299dbc15cdc
Esteban <a01184855@itesm.mx>
2017-03-19 12:31:51 -0600

    Registration user form

---
+++ VIFAC/users/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'users/registrationform.html'
+    
+    # display blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+    # process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+        
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+            
+            #return User if credentials correct
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                
+                if user.is_active:
+                    login(request, user)
+                    return redirect('/')
+        return render(request, self.template_name, {'form': form})
