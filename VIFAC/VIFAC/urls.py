20c550e32a18d3e6b641accce9e71a8b54027b90
Esteban <a01184855@itesm.mx>
2017-03-19 12:29:26 -0600

    Registration user form

--- VIFAC/VIFAC/urls.py
+++ VIFAC/VIFAC/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^donadores/', include('donors.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^usuarios/', include('users.urls'))
 ]
