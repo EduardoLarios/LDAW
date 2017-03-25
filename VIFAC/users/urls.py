622382620ca8c3420af70fa92883aa1703931707
Esteban <a01184855@itesm.mx>
2017-03-24 22:42:00 -0600

    javascript and css

--- VIFAC/users/urls.py
+++ VIFAC/users/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 from .views import UserDelete
 
+
 app_name = 'users'
 
 urlpatterns = [
@@ -10,5 +11,5 @@ urlpatterns = [
     url(r'^registrar/$', views.UserFormView.as_view(), name='register'),
     url(r'^borrar/(?P<pk>\d+)$', UserDelete.as_view(), name='delete'),
     url(r'^lista_usuarios/$', views.list_users, name='lista_usuarios'),
-    url(r'^editar_usuario/(?P<pk>\d+)/$', views.UserUpdate.as_view(), name="user-update")
+    url(r'^editar_usuario/(?P<pk>\d+)/$', views.UserUpdate.as_view(), name="user-update"),
 ]
